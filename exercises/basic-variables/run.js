09b53b0bfa375c694d3cbfd5855739cfa95dc403
Jujhar Singh <jujhar@jujhar.com>
2022-10-30 11:57:44 +0000

    basic variables in place

---
+++ exercises/basic-variables/run.js
@@ -0,0 +1,41 @@
+workingWithIntegers = function () {
+  const myNumber = 1;
+  return myNumber;
+};
+
+workingWithStrings = function () {
+  const myString = "";
+  return myString;
+};
+
+/**
+ * Pure integer arrays
+ */
+workingWithIntArrays = function () {
+  const myArray = [];
+  return myArray;
+};
+
+/**
+ * Arrays can have mixed types in them
+ */
+workingWithMixedArrays = function () {
+  const myArray = [];
+  return myArray;
+};
+
+/**
+ * Javascript objects are cool
+ */
+workingWithObjects = function () {
+  const myObject = { };
+  return myObject;
+};
+
+module.exports = {
+  workingWithIntegers,
+  workingWithStrings,
+  workingWithIntArrays,
+  workingWithMixedArrays,
+  workingWithObjects
+};
---
+++ exercises/basic-variables/run.test.js
@@ -0,0 +1,22 @@
+const assert = require("assert");
+const code = require("./run.js");
+
+describe("Code works", () => {
+  it("workingWithIntegers Works", () => {
+    assert.strictEqual(code.workingWithIntegers(), 42);
+  });
+
+  it("workingWithStrings works", () => {
+    assert.strictEqual(code.workingWithStrings(), "I love potatoes");
+  });
+
+  it("workingWithIntArrays works", () => {
+    assert.deepEqual(code.workingWithIntArrays(), [1, 2, 34, 55, 666, 123412415]);
+  });
+  it("workingWithMixedArrays works", () => {
+    assert.deepEqual(code.workingWithMixedArrays(),  [1, 2, "cabbages", 34, 55, 666, 123412415]);
+  });
+  it("workingWithObjects works", () => {
+    assert.deepEqual(code.workingWithObjects(), { greeting: "Hello", age: 20, city: "Liverpool" });
+  });
+});
